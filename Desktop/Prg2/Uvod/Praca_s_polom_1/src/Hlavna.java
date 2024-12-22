87b258bff10bedb1f75e2afebb0d6d3a12259f9f
berkesadam74 <131783764+berkesadam74@users.noreply.github.com>
2024-12-22 15:27:36 +0100

    Delete Desktop/Prg2 directory
    ...

--- Desktop/Prg2/Uvod/Praca_s_polom_1/src/Hlavna.java
+++
@@ -1,9 +0,0 @@
-public class Hlavna {
-    public static void main(String[] args) {
-        int n = 15;
-
-        Pole cisla = new Pole(n);
-        cisla.napln();
-        cisla.vypis();
-    }
-}
